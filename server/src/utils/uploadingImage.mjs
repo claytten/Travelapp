4ff3c3e6b4fc18ea7f5ab4f5ee34cb8a16484837
Wahyu Aji Sulaiman <wahyuajisulaiman@gmail.com>
2021-02-03 00:43:15 +0700

    DAY 9 - build automatically mkdir folder and changing package uuid

--- server/src/utils/uploadingImage.mjs
+++ server/src/utils/uploadingImage.mjs
@@ -1,13 +1,15 @@
 import multer from 'multer';
-import { uuid } from 'uuidv4';
+import { v4 as uuidv4 } from 'uuid';
 import { extname } from 'path';
 
 export const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'uploads/avatar');
+    const pattern = req.route.path.split('/');
+    const getPath = pattern[pattern.length - 2];
+    cb(null, `uploads/${getPath}`);
   },
   filename(req, file, cb) {
-    cb(null, `${uuid()}-${Date.now()}${extname(file.originalname)}`);
+    cb(null, `${uuidv4()}-${Date.now()}${extname(file.originalname)}`);
   },
 });
 
